00e045eb319fd438eb27be670487bce8c572b462
Your Name <you@example.com>
2025-05-09 17:55:42 +0800

    added devices meta

--- src/ao_registry.erl
+++ src/ao_registry.erl
@@ -1,13 +1,18 @@
 %% File: ao_registry.erl
 -module(ao_registry).
--export([get_module/1]).
+-export([get_module/1, get_all_devices/0]).
 
-get_module(DeviceBin) ->
-    DeviceMap = #{
+%% Internal shared device map
+device_map() ->
+    #{
         <<"hello@1.0">> => dev_hello,
         <<"calculator@1.0">> => dev_calculator,
-        <<"echo@1.0">> => dev_echo
+        <<"echo@1.0">> => dev_echo,
+        <<"meta@1.0">> => dev_meta
+    }.
 
-        
-    },
-    maps:get(DeviceBin, DeviceMap, undefined).
+get_module(DeviceBin) ->
+    maps:get(DeviceBin, device_map(), undefined).
+
+get_all_devices() ->
+    maps:keys(device_map()).
